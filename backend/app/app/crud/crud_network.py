792476618aca91ec8f3b982efe9cf880c495ce4b
Eric Burden <ewb.dev@gmail.com>
2020-07-17 11:30:05 -0500

    Change base class for CRUDNetwork

--- backend/app/app/crud/crud_network.py
+++ backend/app/app/crud/crud_network.py
@@ -3,12 +3,12 @@ from typing import List
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
-from app.crud.base import CRUDBase
+from app.crud.base import CRUDBaseLogging
 from app.models.network import Network
 from app.schemas.network import NetworkCreate, NetworkUpdate
 
 
-class CRUDNetwork(CRUDBase[Network, NetworkCreate, NetworkUpdate]):
+class CRUDNetwork(CRUDBaseLogging[Network, NetworkCreate, NetworkUpdate]):
     pass
 
 

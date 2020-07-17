e2c6cafc3b8499edbbc73eda71b22af73ec0728b
Eric Burden <ewb.dev@gmail.com>
2020-07-16 22:57:33 -0500

    Add a useful comment to prestart.sh

--- backend/app/prestart.sh
+++ backend/app/prestart.sh
@@ -3,6 +3,11 @@
 # Let the DB start
 python /app/app/backend_pre_start.py
 
+# To start fresh with a single revision, delete the .py files in
+# app/alembic/versions, drop the database, and uncomment the 
+# line below for a single run. After that, comment it back.
+# alembic revision --autogenerate -m "First Revision"
+
 # Run migrations
 alembic upgrade head
 

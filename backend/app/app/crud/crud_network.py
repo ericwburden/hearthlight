4b29231e2bcdbbe6dc3101ad58cf7de8a7f9c80b
Eric Burden <ewb.dev@gmail.com>
2020-07-16 22:52:03 -0500

    Add CRUD for networks

---
+++ backend/app/app/crud/crud_network.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models.network import Network
+from app.schemas.network import NetworkCreate, NetworkUpdate
+
+
+class CRUDNetwork(CRUDBase[Network, NetworkCreate, NetworkUpdate]):
+    pass
+
+
+network = CRUDNetwork(Network)

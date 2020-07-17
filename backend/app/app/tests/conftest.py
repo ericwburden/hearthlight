7293b81ce595e71123f929aa96e4611b83ded7e5
Eric Burden <ewb.dev@gmail.com>
2020-07-16 22:50:49 -0500

    Add fixtures, startup, teardown
    Added session fixtures for superuser and normal_user. normal_user creates
    a random user for the session. Also added
    setup and teardown methods to clear the database between tests

--- backend/app/app/tests/conftest.py
+++ backend/app/app/tests/conftest.py
@@ -7,8 +7,10 @@ from sqlalchemy.orm import Session
 from app.core.config import settings
 from app.db.session import SessionLocal
 from app.main import app
-from app.tests.utils.user import authentication_token_from_email
-from app.tests.utils.utils import get_superuser_token_headers
+from app.models.user import User
+from app.models.network import Network
+from app.tests.utils.user import authentication_token_from_email, create_random_user
+from app.tests.utils.utils import get_superuser_token_headers, get_superuser
 
 
 @pytest.fixture(scope="session")
@@ -32,3 +34,31 @@ def normal_user_token_headers(client: TestClient, db: Session) -> Dict[str, str]
     return authentication_token_from_email(
         client=client, email=settings.EMAIL_TEST_USER, db=db
     )
+
+
+@pytest.fixture(scope="module")
+def superuser(client: TestClient) -> User:
+    return get_superuser(client)
+
+
+@pytest.fixture(scope="module")
+def normal_user(client: TestClient) -> User:
+    db = SessionLocal()
+    return create_random_user(db)
+
+
+def clear_db():
+    db = SessionLocal()
+    try:
+        num_rows_deleted = db.query(Network).delete()
+        db.commit()
+    except:
+        db.rollback()
+
+
+def pytest_sessionstart(session):
+    clear_db()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    clear_db()

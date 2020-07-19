4d55e37e602d48fc62b78c5a1854a56bd11ef76d
Eric Burden <ewb.dev@gmail.com>
2020-07-18 23:54:13 -0500

    Correct table naming
    Previously, tables were simple lowercases of the class name,
    now the class names are converted from CamelCase to snake_case

--- backend/app/app/db/base_class.py
+++ backend/app/app/db/base_class.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
@@ -10,4 +11,4 @@ class Base:
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+        return re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__).lower()

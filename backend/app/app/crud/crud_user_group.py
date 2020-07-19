7bb9260c440cbe79848b2fb3ab8d037f5a7131a8
Eric Burden <ewb.dev@gmail.com>
2020-07-19 15:18:21 -0500

    Add CRUD functions to UserGroup
    Get all UserGroup Users, remove User from UserGroup

--- backend/app/app/crud/crud_user_group.py
+++ backend/app/app/crud/crud_user_group.py
@@ -12,11 +12,17 @@ from app.schemas.user_group import UserGroupCreate, UserGroupUpdate
 
 
 class CRUDUserGroup(CRUDBaseLogging[UserGroup, UserGroupCreate, UserGroupUpdate]):
-    def add_user_to_group(self, db: Session, *, user_group: UserGroup, user: User):
+    def add_user_to_group(self, db: Session, *, user_group: UserGroup, user: User) -> UserGroupUser:
         user_group.users.append(user)
         db.commit()
         q = db.query(UserGroupUser).filter(and_(literal_column('user_group_id')==user_group.id, literal_column('user_id')==user.id))
         return q.first()
 
+    def get_users(self, db: Session, *, user_group: UserGroup) -> List[User]:
+        return user_group.users
+
+    def remove_user_from_group(self, db:Session, *, user_group: UserGroup, user: User) -> UserGroup:
+        user_group.users.remove(user)
+        return user_group
 
 user_group = CRUDUserGroup(UserGroup)

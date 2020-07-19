bb8e660e1ad4b14cc7e8349a7ce5dbd126b46736
Eric Burden <ewb.dev@gmail.com>
2020-07-19 15:19:31 -0500

    Add User CRUD function to get UserGroups

--- backend/app/app/crud/crud_user.py
+++ backend/app/app/crud/crud_user.py
@@ -1,16 +1,20 @@
-from typing import Any, Dict, Optional, Union
+from typing import Any, Dict, Optional, Union, List
 
 from sqlalchemy.orm import Session
 
 from app.core.security import get_password_hash, verify_password
 from app.crud.base import CRUDBase
 from app.models.user import User
+from app.models.user_group import UserGroup
 from app.schemas.user import UserCreate, UserUpdate
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
         return db.query(User).filter(User.email == email).first()
+
+    def get_user_groups(self, db: Session, *, user: User) -> List[UserGroup]:
+        return user.user_groups
 
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
         db_obj = User(

d21f0f408e90fca1906b06190dd1908825a724cb
Eric Burden <ewb.dev@gmail.com>
2020-07-20 23:40:21 -0500

    Add CRUD to add permission and get permission for user group

--- backend/app/app/crud/crud_user_group.py
+++ backend/app/app/crud/crud_user_group.py
@@ -7,7 +7,8 @@ from sqlalchemy.sql.expression import literal, literal_column
 
 from app.crud.base import CRUDBaseLogging
 from app.models.user import User
-from app.models.user_group import UserGroup, UserGroupUser
+from app.models.permission import Permission
+from app.models.user_group import UserGroup, UserGroupUser, UserGroupPermission
 from app.schemas.user_group import UserGroupCreate, UserGroupUpdate
 
 
@@ -23,6 +24,24 @@ class CRUDUserGroup(CRUDBaseLogging[UserGroup, UserGroupCreate, UserGroupUpdate]
 
     def remove_user_from_group(self, db:Session, *, user_group: UserGroup, user: User) -> UserGroup:
         user_group.users.remove(user)
+        db.commit()
         return user_group
+
+    def add_permission(self, db: Session, *, user_group: UserGroup, permission: Permission, enabled: bool) -> UserGroupPermission:
+        user_group_permission = UserGroupPermission(user_group_id=user_group.id, permission_id=permission.id, enabled=enabled)
+        db.add(user_group_permission)
+        db.commit()
+        db.refresh(user_group_permission)
+        return user_group_permission
+
+    def get_permission(self, db:Session, *, user_group: UserGroup, permission_id: int) -> Permission:
+        return db.query(Permission).join(UserGroupPermission).join(UserGroup).filter(UserGroup.id==user_group.id).first()
+
+
+    def delete_permission(self, db: Session, *, user_group: UserGroup, permission: Permission) -> UserGroupPermission:
+        user_group_permission = db.query(UserGroupPermission).filter(and_(literal_column('user_group_id')==user_group.id, literal_column('permission_id')==permission.id)).first()
+        db.delete(user_group_permission)
+        db.commit()
+        return user_group_permission
 
 user_group = CRUDUserGroup(UserGroup)

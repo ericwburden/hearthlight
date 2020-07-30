451b7c6061361e4bc88f9ddf2e6e0dbfba429200
Eric Burden <ewb.dev@gmail.com>
2020-07-29 22:44:47 -0500

    Update create_random_node
    Now accepts is_active as an optional parameter to be passed to
    the CRUD function, defaults to True

--- backend/app/app/tests/utils/node.py
+++ backend/app/app/tests/utils/node.py
@@ -13,7 +13,8 @@ def create_random_node(
     *,
     created_by_id: Optional[int] = None,
     node_type: Optional[str] = None,
-    parent_id: Optional[int] = None
+    parent_id: Optional[int] = None,
+    is_active: Optional[bool] = True,
 ) -> models.Node:
     if created_by_id is None:
         user = create_random_user(db)
@@ -21,5 +22,5 @@ def create_random_node(
     if node_type is None:
         node_type = "node"
     name = random_lower_string()
-    node_in = NodeCreate(name=name, node_type=node_type, parent_id=parent_id)
+    node_in = NodeCreate(name=name, node_type=node_type, parent_id=parent_id, is_active=is_active)
     return crud.node.create(db=db, obj_in=node_in, created_by_id=created_by_id)

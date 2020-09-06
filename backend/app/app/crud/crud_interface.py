d42674bcba1c4a52e8605f1f97ad69ee3c3696e1
Eric Burden <ewb.dev@gmail.com>
2020-09-06 15:35:35 -0500

    Add curd function to get an interface by the name of the template table

--- backend/app/app/crud/crud_interface.py
+++ backend/app/app/crud/crud_interface.py
@@ -1,10 +1,18 @@
+from sqlalchemy.orm import Session
+
 from app.crud.base import CRUDBaseLogging
 from app.models.interface import Interface
 from app.schemas.interface import InterfaceCreate, InterfaceUpdate
 
 
 class CRUDInterface(CRUDBaseLogging[Interface, InterfaceCreate, InterfaceUpdate]):
-    pass
+    def get_by_template_table_name(self, db: Session, *, table_name: str) -> Interface:
+        query = db.query(Interface).filter(
+            Interface.table_template["table_name"].astext == table_name
+        )
+        return query.first()
+
+    # TODO: Create CRUD to create the templated backing table
 
 
 interface = CRUDInterface(Interface)

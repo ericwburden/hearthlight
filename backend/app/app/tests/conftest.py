02bfa8432604c14f5ea0834b6265bdbb766dd952
Eric Burden <ewb.dev@gmail.com>
2020-09-08 12:13:46 -0500

    Modify testing setup (conftest.py)
    Modified to drop all tables created by an interface

--- backend/app/app/tests/conftest.py
+++ backend/app/app/tests/conftest.py
@@ -3,15 +3,17 @@ from typing import Dict, Generator
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
+from sqlalchemy import MetaData
 
 from app.core.config import settings
-from app.db.session import SessionLocal
+from app.db.session import SessionLocal, engine
 from app.main import app
 
 from app.models.node import Node
 from app.models.user import User
 from app.models.user_group import UserGroup, UserGroupUserRel, UserGroupPermissionRel
 from app.models.permission import Permission, NodePermission
+from app.models.interface import Interface
 
 from app.tests.utils.user import authentication_token_from_email, create_random_user
 from app.tests.utils.utils import get_superuser_token_headers, get_superuser
@@ -53,6 +55,7 @@ def normal_user(client: TestClient) -> User:
 
 def clear_db():
     db = SessionLocal()
+
     models = [
         UserGroupPermissionRel,
         NodePermission,
@@ -60,10 +63,18 @@ def clear_db():
         UserGroupUserRel,
         UserGroup,
         Node,
+        Interface,
+        User,
     ]
+    model_table_names = [m.__tablename__ for m in models]
+    model_table_names.append('alembic_version')
+    # Drop records from the static tables
     for model in models:
         try:
-            db.query(model).delete()
+            if model.__tablename__ == "user":  # leave the superuser
+                db.query(model).filter(model.id > 1).delete()
+            else:
+                db.query(model).delete()
             # print(f'Deleted {n} {model.__name__}s')
             db.commit()
         except Exception as e:
@@ -71,13 +82,12 @@ def clear_db():
             print(e)
             db.rollback()
 
-    try:
-        db.query(User).filter(User.id > 1).delete()
-        db.commit()
-    except Exception as e:
-        print(e)
-        print("Failed to delete extra users.")
-        db.rollback()
+    # Drop the dynamically created interface tables
+    metadata = MetaData(engine, reflect=True)
+    all_tables = metadata.tables
+    for name, table in all_tables.items():
+        if name not in model_table_names:
+            table.drop()
 
 
 def pytest_sessionstart(session):

babc2e287d628a34b70b93c4d0e962681d17b0ee
Eric Burden <ewb.dev@gmail.com>
2020-09-08 20:32:44 -0500

    Change the metadata bind for generic models

--- backend/app/app/models/generic.py
+++ backend/app/app/models/generic.py
@@ -4,7 +4,8 @@ from app.db.base_class import Base
 
 
 def get_generic_model(table_name: str) -> Base:
+    # breakpoint()
     metadata = MetaData()
-    metadata.bind = engine
+    metadata.bind = Base
     metadata.reflect(bind=engine)
     return type(table_name, (Base,), {"__table__": metadata.tables[table_name]})

66316f3134eb3ebaf50e1a86e33b3de2e07daf11
Eric Burden <ewb.dev@gmail.com>
2020-09-22 08:30:14 -0500

    Keep changes to prestart.sh

--- backend/app/prestart.sh
+++ backend/app/prestart.sh
@@ -3,10 +3,10 @@
 # Let the DB start
 python /app/app/backend_pre_start.py
 
-# To start fresh with a single revision, delete the .py files in
-# app/alembic/versions, drop the database, and uncomment the 
-# line below for a single run. After that, comment it back.
-# alembic revision --autogenerate -m "First Revision"
+# To retain alembic migrations between restarts, comment the
+# two lines below
+rm -f /app/alembic/versions/*.py
+alembic revision --autogenerate -m "First Revision"
 
 # Run migrations
 alembic upgrade head

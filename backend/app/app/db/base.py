562427b0fe78a6428fc1ec20bad3323de4adecf1
Eric Burden <eric.w.burden@gmail.com>
2020-09-28 21:52:57 -0500

    Rearrange base model imports
    Added an explanatory comment for the rearrangement

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -1,21 +1,27 @@
 # Import all the models, so that Base has them before being
-# imported by Alembic
+# imported by Alembic. The order of these is important to the
+# testing framework. conftest.py imports the models from here
+# (in order) and deletes records from the database prior to
+# testing. If all models containing a foreign key to a table
+# are not deleted before the table referenced, then the pre-testing
+# delete operation will fail and records will accumulate in the
+# database
 from app.db.base_class import Base  # noqa F401
-from app.models.node import Node  # noqa F401
-from app.models.user import User  # noqa F401
 from app.models.user_group import (  # noqa F401
-    UserGroup,  # noqa F401
-    UserGroupUserRel,  # noqa F401
-    UserGroupPermissionRel,  # noqa F401
+    UserGroupUserRel,
+    UserGroupPermissionRel,
+    UserGroup,
 )
+from app.models.user import User  # noqa F401
 from app.models.permission import (  # noqa F401
-    Permission,
     NodePermission,
     InterfacePermission,
+    Permission,
 )
 from app.models.interface import (  # noqa F401
-    Interface,
     FormInputInterface,
     QueryInterface,
     InterfaceNodeRel,
+    Interface,
 )
+from app.models.node import Node  # noqa F401

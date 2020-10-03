31f2a76553bae80a2af857f1a413fac35ebf562b
Eric Burden <eric.w.burden@gmail.com>
2020-10-03 12:44:28 -0500

    Modify order of elements on config page
    Now the edit form will appear on top on smaller screens, and to the
    right on larger screens.

--- frontend2/src/views/admin/ConfigureApplication.vue
+++ frontend2/src/views/admin/ConfigureApplication.vue
@@ -11,78 +11,88 @@
       <v-spacer></v-spacer>
     </v-row>
     <v-row>
-      <v-col>
-        <h2>Networks</h2>
-        <v-treeview
-          :items="items"
-          :active.sync="active"
-          :open.sync="open"
-          activatable
-          color="info"
-          transition
-          expand-icon="mdi-arrow-down-drop-circle"
-          return-object
-          ><template v-slot:prepend="{ item }">
-            <IconWithTooltip v-if="item.type == 'network'" icon="mdi-google-circles-extended" msg="Network" />
-            <IconWithTooltip v-if="item.type == 'node'" icon="mdi-circle-double" msg="Node" />
-            <IconWithTooltip v-if="item.type == 'user_group'" icon="mdi-account-supervisor-circle" msg="User Group" />
-            <IconWithTooltip v-if="item.type == 'interface'" icon="mdi-swap-vertical-circl" msg="Interface" />
-          </template>
-          <template v-slot:append="{ item }">
-            <!-- Node Button -->
-            <v-tooltip bottom v-if="['network', 'node'].includes(item.type)">
-              <template v-slot:activator="{ on, attrs }">
-                <v-sheet v-bind="attrs" v-on="on" class="d-inline">
-                  <v-menu offset-y>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn v-if="item.id" icon color="success" v-bind="attrs" v-on="on">
-                        <v-icon>mdi-playlist-plus</v-icon>
-                      </v-btn>
-                    </template>
-                    <v-list>
-                      <v-list-item @click="addNodeChild(item)">Node</v-list-item>
-                      <v-list-item @click="selectItem(item)">User Group</v-list-item>
-                      <v-list-item @click="selectItem(item)">Interface</v-list-item>
-                    </v-list>
-                  </v-menu>
-                </v-sheet>
-              </template>
-              <span>Add Child</span>
-            </v-tooltip>
-
-            <!-- User Group Button -->
-            <v-tooltip bottom v-if="item.type == 'user_group'">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn v-if="item.id" @click="selectItem(item)" icon color="success" v-bind="attrs" v-on="on">
-                  <v-icon>mdi-account-plus</v-icon>
-                </v-btn>
-              </template>
-              <span>Add User</span>
-            </v-tooltip>
-
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn v-if="item.id" @click="updateItem(item)" icon color="info" v-bind="attrs" v-on="on">
-                  <v-icon>mdi-circle-edit-outline</v-icon>
-                </v-btn>
-              </template>
-              <span>Edit</span>
-            </v-tooltip>
-
-            <v-tooltip bottom>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn v-if="item.id" @click="deleteItem(item)" icon color="error" v-bind="attrs" v-on="on">
-                  <v-icon>mdi-delete</v-icon>
-                </v-btn>
-              </template>
-              <span>Delete</span>
-            </v-tooltip>
-          </template>
-        </v-treeview>
+      <v-col md="6" sm="12" order="1" order-md="2">
+        <v-card>
+          <router-view :key="$route.fullPath"></router-view>
+        </v-card>
       </v-col>
-      <v-divider class="mx-4" vertical></v-divider>
-      <v-col>
-        <router-view :key="$route.fullPath"></router-view>
+      <v-col md="6" sm="12" order="2" order-md="1">
+        <v-card>
+          <v-container>
+            <h2>Networks</h2>
+            <v-treeview
+              :items="items"
+              :active.sync="active"
+              :open.sync="open"
+              activatable
+              color="info"
+              transition
+              expand-icon="mdi-arrow-down-drop-circle"
+              return-object
+              ><template v-slot:prepend="{ item }">
+                <IconWithTooltip v-if="item.type == 'network'" icon="mdi-google-circles-extended" msg="Network" />
+                <IconWithTooltip v-if="item.type == 'node'" icon="mdi-circle-double" msg="Node" />
+                <IconWithTooltip
+                  v-if="item.type == 'user_group'"
+                  icon="mdi-account-supervisor-circle"
+                  msg="User Group"
+                />
+                <IconWithTooltip v-if="item.type == 'interface'" icon="mdi-swap-vertical-circl" msg="Interface" />
+              </template>
+              <template v-slot:append="{ item }">
+                <!-- Node Button -->
+                <v-tooltip bottom v-if="['network', 'node'].includes(item.type)">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-sheet v-bind="attrs" v-on="on" class="d-inline">
+                      <v-menu offset-y>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-btn v-if="item.id" icon color="success" v-bind="attrs" v-on="on">
+                            <v-icon>mdi-playlist-plus</v-icon>
+                          </v-btn>
+                        </template>
+                        <v-list>
+                          <v-list-item @click="addNodeChild(item)">New Node</v-list-item>
+                          <v-list-item @click="selectNodeChild(item)">Existing Node</v-list-item>
+                          <v-list-item @click="selectItem(item)">User Group</v-list-item>
+                          <v-list-item @click="selectItem(item)">Interface</v-list-item>
+                        </v-list>
+                      </v-menu>
+                    </v-sheet>
+                  </template>
+                  <span>Add Child</span>
+                </v-tooltip>
+
+                <!-- User Group Button -->
+                <v-tooltip bottom v-if="item.type == 'user_group'">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn v-if="item.id" @click="selectItem(item)" icon color="success" v-bind="attrs" v-on="on">
+                      <v-icon>mdi-account-plus</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Add User</span>
+                </v-tooltip>
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn v-if="item.id" @click="updateItem(item)" icon color="info" v-bind="attrs" v-on="on">
+                      <v-icon>mdi-circle-edit-outline</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Edit</span>
+                </v-tooltip>
+
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn v-if="item.id" @click="deleteItem(item)" icon color="error" v-bind="attrs" v-on="on">
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Delete</span>
+                </v-tooltip>
+              </template>
+            </v-treeview>
+          </v-container>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -161,6 +171,10 @@ export default class ConfigureApplication extends Vue {
     commitSetConfigureNodeFormProps(this.$store, configureNodeFormProps);
 
     this.$router.push(`/admin/configure/node/${uuidv4()}`);
+  }
+
+  public selectNodeChild(node: ApplicationModelEntry) {
+    this.$router.push(`/admin/configure/node/${node.id}/child-search`);
   }
 
   public updateItem(item: ApplicationModelEntry) {

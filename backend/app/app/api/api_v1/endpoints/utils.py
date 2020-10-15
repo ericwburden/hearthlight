8d8422ffc42f75150ed0426b55e511dc2ecda311
Eric Burden <eric.w.burden@gmail.com>
2020-10-14 20:30:49 -0500

    Adds two utility endpoints
    - Endpoint to return a list of database table names
    - Endpoint to return a list of table column names for a given table

--- backend/app/app/api/api_v1/endpoints/utils.py
+++ backend/app/app/api/api_v1/endpoints/utils.py
@@ -1,12 +1,14 @@
 from typing import Any, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic.networks import EmailStr
+from sqlalchemy import inspect
 from sqlalchemy.orm import Session
 
 from app import models, schemas, crud
 from app.api import deps
 from app.core.celery_app import celery_app
+from app.db.session import engine
 from app.utils import send_test_email
 
 router = APIRouter()
@@ -39,3 +41,18 @@ def test_email(
 @router.get("/node-types/", response_model=List[str], status_code=200)
 def get_node_types(db: Session = Depends(deps.get_db)) -> List[str]:
     return crud.node.get_types(db)
+
+
+@router.get("/table-names/", response_model=List[str], status_code=200)
+def get_node_types() -> List[str]:
+    inspector = inspect(engine)
+    return inspector.get_table_names()
+
+
+@router.get("/column-names/{table_name}", response_model=List[str], status_code=200)
+def get_node_types(table_name: str) -> List[str]:
+    inspector = inspect(engine)
+    if table_name not in inspector.get_table_names():
+        raise HTTPException(status_code=404, detail=f"No table named {table_name}.")
+    columns = inspector.get_columns(table_name)
+    return [c.get("name") for c in columns]
